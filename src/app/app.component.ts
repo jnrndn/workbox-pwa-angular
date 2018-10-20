20cea822c2f148c850020eed0bbc4d665b9c2a30
Juan José Rendón <juan.rendon@yuxiglobal.com>
2018-10-20 15:31:52 -0500

    feat: fetch data from api and display some of it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { RickNMortyService } from './services/rick-n-morty.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: [ './app.component.scss' ],
 })
-export class AppComponent {
-  title = 'workboxpwa';
+export class AppComponent implements OnInit {
+  characteres$: Observable<any>;
+
+  constructor(private ricknmorty: RickNMortyService) { }
+
+  ngOnInit() {
+    this.characteres$ = this.ricknmorty.getCharacteres();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
@@ -9,8 +10,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [ AppComponent ],
 })
 export class AppModule { }
